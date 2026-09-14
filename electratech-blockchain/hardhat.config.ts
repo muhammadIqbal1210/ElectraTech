55435cbf20b6f3e29ad33a32b1642b24c7e52b03
muhammadIqbal1210 <iqbalmuhammad07015@gmail.com>
2026-09-14 13:30:29 +0700

    fix:perbaiki alamat polygon

--- electratech-blockchain/hardhat.config.ts
+++ electratech-blockchain/hardhat.config.ts
@@ -29,14 +29,14 @@ export default defineConfig({
     // 1. Polygon Amoy (Testnet)
     polygonAmoy: {
       type: "http",
-      url: process.env.POLYGON_AMOY_RPC_URL || "https://rpc-amoy.polygon.technology",
+      url: process.env.POLYGON_AMOY_RPC_URL || "https://polygon-amoy.drpc.org",
       accounts: process.env.POLYGON_PRIVATE_KEY ? [process.env.POLYGON_PRIVATE_KEY] : [],
     },
 
     // 2. Polygon Mainnet (Production)
     polygonMainnet: {
       type: "http",
-      url: process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-rpc.com",
+      url: process.env.POLYGON_MAINNET_RPC_URL || "https://polygon.drpc.org",
       accounts: process.env.POLYGON_PRIVATE_KEY ? [process.env.POLYGON_PRIVATE_KEY] : [],
     },
   },
