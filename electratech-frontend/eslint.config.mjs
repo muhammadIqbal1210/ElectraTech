a6ca008c7171111f16ec5c85c963536f3d52db0a
muhammadIqbal1210 <iqbalmuhammad07015@gmail.com>
2026-09-18 09:05:47 +0700

    chore:update rules eslint

--- electratech-frontend/eslint.config.mjs
+++ electratech-frontend/eslint.config.mjs
@@ -5,9 +5,20 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/preserve-manual-memoization": "off",
+      "react-hooks/purity": "off",
+
+      "@next/next/no-img-element": "warn",
+    },
+  },
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
